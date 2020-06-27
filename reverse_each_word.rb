482685ccd459bf23374887edc75334f34bdfc7c1
Michael R <mruhaak@gmail.com>
2020-06-27 05:05:48 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,3 +5,7 @@ end
 def reverse_each_word(sentence)
   sentence.reverse.split.reverse.join(" ")
 end
+
+def reverse_each_word(sentence)
+  sentence.reverse.collect {|word| word.reverse}.join(" ")
+end
